6de0a3cb209a031366f0f2b848b5f96b83313eed
ThatOKPerson <oran.kelly@kainos.com>
2023-08-31 14:37:36 +0100

    005-View-All-Sales-Employees

--- src/main/java/org/kainos/ea/resources/SalesEmployeeController.java
+++ src/main/java/org/kainos/ea/resources/SalesEmployeeController.java
@@ -3,9 +3,12 @@ package org.kainos.ea.resources;
 import io.swagger.annotations.Api;
 import org.kainos.ea.api.SalesEmployeeService;
 import org.kainos.ea.cli.SalesEmployee;
+import org.kainos.ea.cli.SalesEmployeeRequest;
 import org.kainos.ea.client.FailedToCreateSalesEmployeeException;
+import org.kainos.ea.client.FailedToGetSalesEmployeeException;
 import org.kainos.ea.client.InvalidSalesEmployeeException;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -19,10 +22,22 @@ import javax.ws.rs.core.Response;
 public class SalesEmployeeController {
     private SalesEmployeeService service = new SalesEmployeeService();
 
+    @GET
+    @Path("/orders")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getSalesEmployees(){
+        try {
+            return Response.ok(service.getAllSalesEmployees()).build();
+        } catch (FailedToGetSalesEmployeeException e) {
+            System.err.println(e.getMessage());
+            return Response.serverError().build();
+        }
+    }
+
     @POST
     @Path("/salesEmployees")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createSalesEmployee(SalesEmployee salesEmployee){
+    public Response createSalesEmployee(SalesEmployeeRequest salesEmployee){
         try {
             return Response.ok(service.createSalesEmployee(salesEmployee)).build();
         } catch (FailedToCreateSalesEmployeeException e) {
